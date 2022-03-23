4dd448807a17042d07ec2e25c21021e5bf0a0e3f
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-03-22 20:12:56 -0600

    Agregando evento window

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@ import { valida } from "../js/validaciones.js";
 
 const inputs = document.querySelectorAll("input");
 
+window.addEventListener("load", function (event){
+    event.preventDefault();
+});
+
 inputs.forEach((input) => {
     input.addEventListener("blur", (input) => {
         valida(input.target);

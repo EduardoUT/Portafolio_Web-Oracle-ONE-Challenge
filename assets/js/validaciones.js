cc4b18a41d82e3ff61697b4be306073d01a85af1
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-03-22 19:06:39 -0600

    Agregando función valida, objeto validadores y función validarNombre

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -1,2 +1,14 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.campo;
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);
+    }
+}
 
+const validadores = {
+    nombre: (input) => validarNombre(input),
+};
 
+function validarNombre(input) {
+    
+}

02ccb2746fbc1823a4eb928ae8a0dcdade637dc8
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-03-25 13:31:06 -0600

    Agregando evento blur a textarea y llamando función para validarlo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
 import { valida } from "../js/validaciones.js";
+import { validaTextArea } from "../js/validaciones.js";
 
 const inputs = document.querySelectorAll("input");
+const textArea = document.querySelector("textarea");
 
 window.addEventListener("load", function (event){
     event.preventDefault();
+});
+
+textArea.addEventListener("blur", (textArea) => {
+    validaTextArea(textArea.target);
 });
 
 inputs.forEach((input) => {

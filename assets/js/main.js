21d931a1878363e90b097d00e7c445f29b9cada7
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-03-26 19:38:10 -0600

    Comentando líneas de código

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,42 @@
 import { valida } from "../js/validaciones.js";
 import { validaTextArea } from "../js/validaciones.js";
 
+/**
+ * @const 
+ * Contiene todos los inputs existentes
+ */
 const inputs = document.querySelectorAll("input");
+/**
+ * @const
+ * Contiene el elemento textarea en el formulario.
+ */
 const textArea = document.querySelector("textarea");
 
-window.addEventListener("load", function (event){
-    event.preventDefault();
-});
 
+window.addEventListener("load",
+    /**
+     * @function
+     * Evita que el formulario se envíe al cargar la
+     * página.
+     */
+    function (event) {
+        event.preventDefault();
+    }
+);
+
+/**
+ * Permite validar el textarea una vez el
+ * campo pierde el foco.
+ */
 textArea.addEventListener("blur", (textArea) => {
     validaTextArea(textArea.target);
 });
 
+/**
+ * Realiza un ciclo por los inputs y 
+ * de forma individual valida en el caso 
+ * de que se pierda el foco.
+ */
 inputs.forEach((input) => {
     input.addEventListener("blur", (input) => {
         valida(input.target);

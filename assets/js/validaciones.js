d185f938b0d3596baa310f76eeeb06cc3c30dcc2
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-03-27 11:20:47 -0600

    Comentando console.log

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -107,9 +107,11 @@ function mostrarMensajeDeError(tipoDeInput, input) {
     let mensaje = "";
     tiposDeErrores.forEach((error) => {
         if (input.validity[error]) {
+            /*
             console.log(tipoDeInput, error);
             console.log(input.validity[error]);
             console.log(mensajesDeError[tipoDeInput][error]);
+            */
             mensaje = mensajesDeError[tipoDeInput][error];
         }
     });

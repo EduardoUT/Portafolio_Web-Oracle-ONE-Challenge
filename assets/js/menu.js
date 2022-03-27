d92b6f6586b0384965c115e660dc9b9055d78d20
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-03-27 11:40:43 -0600

    Agregando comentarios en funciones ocultarMenu y mostrarMenu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -71,3 +71,90 @@ window.addEventListener("scroll", () => {
 George Martsoukos
 https://webdesign.tutsplus.com/es/tutorials/how-to-hide-reveal-a-sticky-header-on-scroll-with-javascript--cms-33756
 */
+
+
+/**
+ * 
+ * @param {Object} inputs
+ * Elementos input del DOM en el formulario. 
+ * @param {Object} textArea 
+ * Elemento textarea del DOM en el formulario.
+ * @param {Object} mediaQueryMin
+ * Objeto que devuelve un valor boleano en caso de cumplirse el media query, para
+ * valores min-width.
+ * @param {Object} mediaQueryMax 
+ * Objeto que devuelve un valor boleano en caso de cumplirse el media query, para
+ * valores max-width.
+ * @returns void.
+ * 
+ * @function 
+ * Cuando cualquier campo este seleccionado y contenga el foco,
+ * el menú automáticamente se ocultará en inputs y textarea del formulario.
+ * 
+ * La condición permite validar un rango entre min-width y max-width en pixeles del objeto
+ * MediaQueryList.
+ * 
+ */
+export function ocultarMenu(inputs, textArea, mediaQueryMin, mediaQueryMax) {
+
+    if (mediaQueryMin.matches && mediaQueryMax.matches) {
+
+        inputs.forEach((input) => {
+            input.addEventListener("focus", function () {
+                body.classList.remove(scrollArriba);
+                body.classList.add(scrollAbajo);
+            });
+        });
+
+        textArea.addEventListener("focus", function () {
+            body.classList.remove(scrollArriba);
+            body.classList.add(scrollAbajo);
+        });
+        return;
+    }
+}
+
+/**
+ * 
+ * @param {Object} inputs
+ * Elementos input del DOM en el formulario. 
+ * @param {Object} textArea 
+ * Elemento textarea del DOM en el formulario.
+ * @param {Object} mediaQueryMin
+ * Objeto que devuelve un valor boleano en caso de cumplirse el media query, para
+ * valores min-width.
+ * @param {Object} mediaQueryMax 
+ * Objeto que devuelve un valor boleano en caso de cumplirse el media query, para
+ * valores max-width.
+ * @returns void.
+ * 
+ * @function 
+ * Cuando cualquier campo no este seleccionado y pierda el foco,
+ * el menú automáticamente se ocultará en inputs y textarea del formulario 
+ * esto a fin de evitar abarcar toda la pantalla mientras el teclado en Android, iOS, etc,
+ * se encuentra abierto en dispositivos móviles orientados horizontalmente.
+ * 
+ * La condición permite validar un rango entre min-width y max-width en pixeles del objeto
+ * MediaQueryList.
+ * 
+ */
+export function mostrarMenu(inputs, textArea, mediaQueryMin, mediaQueryMax) {
+    if (mediaQueryMin.matches && mediaQueryMax.matches) {
+        /**
+        * Cuando cualquier campo este seleccionado y contenga el foco,
+        * el menú autoáticamente se ocultará.
+        */
+        inputs.forEach((input) => {
+            input.addEventListener("focusout", function () {
+                body.classList.remove(scrollAbajo);
+                body.classList.add(scrollArriba);
+            });
+        });
+
+        textArea.addEventListener("focusout", function () {
+            body.classList.remove(scrollAbajo);
+            body.classList.add(scrollArriba);
+        });
+        return;
+    }
+}

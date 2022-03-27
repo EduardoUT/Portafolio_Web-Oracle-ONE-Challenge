92d3284022dc3afd88bc6368458ed17900165c97
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-03-27 12:01:13 -0600

    Importando función mostrarOcultarMenu y comentando nuevas variables para mediaQuery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,46 @@
 import { valida } from "../js/validaciones.js";
 import { validaTextArea } from "../js/validaciones.js";
+import { mostrarOcultarMenu } from "./menu.js";
 
 /**
  * @const 
  * Contiene todos los inputs existentes
  */
 const inputs = document.querySelectorAll("input");
+
 /**
  * @const
  * Contiene el elemento textarea en el formulario.
  */
 const textArea = document.querySelector("textarea");
 
+/**
+ * @let 
+ * Devuelve un valor boleano, en caso de cumplirse la coincidencia (true)
+ * de tamaño mínimo en dispositivos de 768px.
+ */
+let mediaQueryMin = window.matchMedia("(min-width: 768px)");
+
+/**
+ * @let 
+ * Devuelve un valor boleano, en caso de cumplirse la coincidencia (true)
+ * de tamaño máximo en dispositivos de 1080px.
+ */
+let mediaQueryMax = window.matchMedia("(max-width: 1080px)");
 
 window.addEventListener("load",
     /**
      * @function
      * Evita que el formulario se envíe al cargar la
      * página.
+     * 
+     * Contiene las siguientes funciones:
+     * @function ocultarMenu
+     * @function mostrarMenu
      */
     function (event) {
         event.preventDefault();
+        mostrarOcultarMenu(inputs, textArea, mediaQueryMin, mediaQueryMax);
     }
 );
 

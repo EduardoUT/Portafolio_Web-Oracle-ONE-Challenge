47d567620e247e0a93b070a258668f543eb90056
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-03-28 17:03:21 -0600

    Corrigiendo comentarios

--- assets/js/habilitarBoton.js
+++ assets/js/habilitarBoton.js
@@ -3,8 +3,10 @@
  * Permite validar si todos los campos del formulario cumplan
  * con las propiedades del objeto validity.
  * 
- * El botón enviar se mostrará inhabilitado en caso de estar vacio al menos un campo,
- * se agregará la propiedad disabled por medio de setAttribute y el estilo css
+ * El botón enviar se mostrará inhabilitado en caso de que al menos un campo
+ * no cumpla las propiedades del objeto validity.
+ * 
+ * Se agregará la propiedad disabled por medio de setAttribute y el estilo css
  * que anula el pointer-event, además de pintar el borde y fondo de color gris.
  * 
  * Si todos los campos del formulario son validos con el objeto
@@ -24,9 +26,9 @@ export function desbloquearBotonEnviar() {
 
     if (nombreValido && correoValido && asuntoValido && mensajeValido) {
         btnEnviar.removeAttribute("disabled");
-        btnEnviar.classList.remove("deshabilitar-btn");
+        btnEnviar.classList.remove("btn--bloqueado");
     } else {
         btnEnviar.setAttribute("disabled", "true");
-        btnEnviar.classList.add("deshabilitar-btn");
+        btnEnviar.classList.add("btn-bloqueado");
     }
 }

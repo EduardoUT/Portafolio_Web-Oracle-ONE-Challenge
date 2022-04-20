bc8b553222beb84f16e9c97f36485cd700e8a887
Eduardo <eduardoreyeshernandez98@gmail.com>
2022-04-20 14:42:43 -0500

    Creando directorio de componentes de página

---
+++ assets/js/componentes/mediaQuery.js
@@ -0,0 +1,20 @@
+/**
+ * Función útil al implementarse en funciones con condiciones lógicas
+ * que permitan modificar el contenido de la página que no sean
+ * posibles de modificar con CSS p.ej.
+ * 
+ * if (mediaQueryCelular()) {código a ejecutar para celulares} else {código a ejecutarse fuera del rango}
+ * @returns {boolean} Si el resultado es true, el contenido cambiara para
+ * dispositivos móviles entre 667px a 1080px cuando estén inclinados horizintalmente.
+ */
+ const mediaQueryCelular = () => {
+    const mediaQueryCelular = window.matchMedia("(min-width: 667px) and (max-width: 1080px)");
+    const anchoValido = (mediaQueryCelular.matches);
+    if (anchoValido) {
+        return anchoValido;
+    } else {
+        return anchoValido;
+    }
+}
+
+export default mediaQueryCelular;
